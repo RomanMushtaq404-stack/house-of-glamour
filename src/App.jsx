d01c0b9fa1b83efacc3c57243c94605854a3f35c
RomanMushtaq404-stack <mushtaqroman404@gmail.com>
2026-07-17 10:39:34 +0500

    Fix App.jsx imports and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,31 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import Hijabs from "./pages/Hijabs";
-import Abayas from "./pages/Abayas";
-import Dresses from "./pages/Dresses";
-import Bags from "./pages/Bags";
-import Shoes from "./pages/Shoes";
-import Cosmetics from "./pages/Cosmetics";
-import Customized from "./pages/Customized";
-
+import Home from "./pages/home";
+import Hijabs from "./pages/hijabs";
+import Abayas from "./pages/abayas";
+import Dresses from "./pages/dresses";
+import Bags from "./pages/bags";
+import Shoes from "./pages/shoes";
+import Cosmetics from "./pages/cosmetics";
+import Customized from "./pages/customized";
 
 function App() {
-
   return (
-
     <BrowserRouter>
-
       <Navbar />
-
       <Routes>
-
         <Route path="/" element={<Home />} />
-
         <Route path="/hijabs" element={<Hijabs />} />
-
         <Route path="/abayas" element={<Abayas />} />
-
         <Route path="/dresses" element={<Dresses />} />
-
         <Route path="/bags" element={<Bags />} />
-
         <Route path="/shoes" element={<Shoes />} />
-
         <Route path="/cosmetics" element={<Cosmetics />} />
-
         <Route path="/customized" element={<Customized />} />
-
       </Routes>
-
     </BrowserRouter>
-
   );
 }
-
 
 export default App;
